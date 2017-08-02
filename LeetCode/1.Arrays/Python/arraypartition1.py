da7e91c1a94f3bfa158037684d91b8a94a8ed14f
Prashanth <prashanthbalasubramani@gmail.com>
2017-08-01 22:18:46 -0400

    Arrays Solution

--- LeetCode/1.Arrays/Python/arraypartition1.py
+++ LeetCode/1.Arrays/Python/arraypartition1.py
@@ -1,5 +1,16 @@
-def arrayPairSum(self, nums):
+def arrayPairSum(nums):
     """
     :type nums: List[int]
     :rtype: int
     """
+    val = 0
+    nums.sort()
+    for i in range(0, len(nums), 2):
+        val += min(nums[i:i+2])
+    return val
+
+
+nums = raw_input()
+nums = nums.split()
+nums_arr = [int(i) for i in nums]
+print arrayPairSum(nums_arr)
