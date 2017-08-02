be50515fd375a301d495e4aad83498d3ecaa47f0
monish appusamy <monishappusamy@gmail.com>
2017-08-01 21:18:20 -0400

    Initial commit for c# project

---
+++ LeetCode1Arrays/LeetCode1Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCode1Arrays
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
