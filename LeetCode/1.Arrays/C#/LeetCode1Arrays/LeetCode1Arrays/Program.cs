b9520fc7e26a4f4abf6f9963f13b492cf4d6ba13
monish appusamy <monishappusamy@gmail.com>
2017-08-01 22:13:36 -0400

    561. Array Partition I

--- LeetCode/1.Arrays/C#/LeetCode1Arrays/LeetCode1Arrays/Program.cs
+++ LeetCode/1.Arrays/C#/LeetCode1Arrays/LeetCode1Arrays/Program.cs
@@ -6,7 +6,21 @@ namespace LeetCode1Arrays
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(ArrayPairSum(new int[]{1,2,4,3,5,6}));
         }
+
+		public static int ArrayPairSum(int[] nums)
+		{
+            Array.Sort(nums);
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    sum += nums[i]; 
+                }
+            }
+            return sum;
+		} 
     }
 }
