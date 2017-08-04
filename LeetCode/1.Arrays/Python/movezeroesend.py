f8df19224174d0dfa94e042f5ab477d4cf291575
Prashanth <prashanthbalasubramani@gmail.com>
2017-08-04 00:10:30 -0400

    Moves Zeroes to End

---
+++ LeetCode/1.Arrays/Python/movezeroesend.py
@@ -0,0 +1,19 @@
+"""
+
+"""
+
+
+def moveZeroes(nums):
+    """
+    :type nums: List[int]
+    :rtype: void Do not return anything, modify nums in-place instead.
+    """
+    count = 0
+    for i in range(len(nums)):
+        
+
+
+nums = [0, 1, 0, 3, 12]
+print "Before : ", nums
+moveZeroes(nums)
+print "After : ", nums
